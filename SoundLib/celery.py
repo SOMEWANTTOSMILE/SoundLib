53677f9273cfd30cb6c77d8df85cb419606ad535
somewanttosmile <dead5moon@gmail.com>
2024-06-17 21:57:20 +0300

    add celery to my projects, create test task(i will delete unnecessary task)

---
+++ SoundLib/celery.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SoundLib.settings')
+
+app = Celery('SoundLib')  # Replace 'your_project' with your project's name.
+
+# Configure Celery using settings from Django settings.py.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load tasks from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.beat_schedule = {
+    'every': {
+        'task': 'premium.tasks.some_scheduled_task',
+        'schedule': 10
+    }
+}
